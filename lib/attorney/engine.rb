1003b227f51e4944090bb1784489af911a2e7bdc
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2018-02-08 14:12:46 -0300

    feat: add CKEditor rich text editor

--- lib/attorney/engine.rb
+++ lib/attorney/engine.rb
@@ -5,5 +5,9 @@ module Attorney
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    initializer 'attorney.assets.precompile' do |app|
+      app.config.assets.precompile += %w( application.js application.css )
+    end
   end
 end
