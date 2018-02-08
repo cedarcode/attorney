b4eda61ecab429825f79d85b39a82ded403deafe
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2018-02-08 17:38:02 -0300

    style: add missing ;

--- app/assets/javascripts/attorney/rich_editor.js
+++ app/assets/javascripts/attorney/rich_editor.js
@@ -6,13 +6,13 @@
 
     var findTextAreas = function() {
       return document.querySelectorAll("textarea[data-rich-editor='true']");
-    }
+    };
 
     var renderEditors = function(inputFields) {
       for (i = 0; i < inputFields.length; i++) {
         CKEDITOR.replace(inputFields[i].getAttribute("id"));
       }
-    }
+    };
   };
 
   document.addEventListener("DOMContentLoaded", function() {
