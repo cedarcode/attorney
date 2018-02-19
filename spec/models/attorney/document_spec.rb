9ff4a7989b620bb4a991372efaef4b5152782922
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2018-02-19 17:46:09 -0300

    feat: validate slug format

--- spec/models/attorney/document_spec.rb
+++ spec/models/attorney/document_spec.rb
@@ -4,7 +4,18 @@ module Attorney
   RSpec.describe Document, type: :model do
     subject(:document) { Document.new(slug: 'terms', content: 'Lorem ipsum dolor sit amet...') }
 
-    it { is_expected.to validate_uniqueness_of(:slug) }
-    it { is_expected.to validate_presence_of(:slug) }
+    context 'validations' do
+      it { is_expected.to validate_uniqueness_of(:slug) }
+      it { is_expected.to validate_presence_of(:slug) }
+
+      context 'slug format' do
+        let(:document) { Document.new(slug: 'terms and conditions') }
+
+        it 'validates format' do
+          expect(document).not_to be_valid
+          expect(document.errors[:slug]).to eq ["Slug format is invalid, try \"terms-and-conditions\" instead."]
+        end
+      end
+    end
   end
 end
