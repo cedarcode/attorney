4c0206f36f5567b5362fcfbca097b94d9a2f1e82
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2018-02-20 16:41:48 -0300

    feat: use ENV variables for setting HTTP auth

--- lib/attorney.rb
+++ lib/attorney.rb
@@ -1,21 +1,10 @@
 require 'attorney/engine'
 
 module Attorney
-  def self.basic_auth
-    settings[:admin][:basic_auth]
-  end
-
-  def self.settings
-    @settings ||= begin
-      config = YAML.load_file(Rails.root.join('config', 'attorney.yml'))[Rails.env]
-
-      if config
-        config.with_indifferent_access
-      else
-        raise "There's been an error while attempting to load config/attorney.yml for the #{Rails.env} environment. Make sure you have provided the proper configuration for #{Rails.env}."
-      end
-    end
-  rescue SystemCallError
-    raise "config/attorney.yml is missing. Please add the file in your application, check attorney's README for details."
+  def self.http_auth
+    {
+      name:     ENV['ATTORNEY_HTTP_AUTH_NAME'] || 'attorney',
+      password: ENV['ATTORNEY_HTTP_AUTH_PASSWORD'] || 'secret'
+    }
   end
 end
